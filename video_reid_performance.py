7a1f6597377ca3c9dec18f11c6d8a5156bf64a4a
dogqi <sq008@mail.ustc.edu.cn>
2022-01-07 13:36:59 +0000

    add some comments on evalution module

--- video_reid_performance.py
+++ video_reid_performance.py
@@ -11,16 +11,16 @@ def compute_AP(good_index, junk_index, order):
     Compute Average Precision(AP) and CMC for one query.\\
     AP is defined as the area under the Precision-Recall(PR) curve.
 
-    params:
+    **params**:
     good_index: 1-d array; indexs that record all good prediction;\\
     junk_index: 1-d array; indexs that record all junk predictions:\\
     order: 1-d array; ordered indexs; 
 
-    return:
+    **return**:
     ap: a scalar;\\
     cmc: 1-d array; size(cmc) = (order,);
 
-    example: 
+    **example**: \\
     a list with good/junk prediction. (1: good; 0: junk) \\
     order = [1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0]\\
     good_index == [0, 1, 3, 4, 7,];\\
@@ -37,8 +37,8 @@ def compute_AP(good_index, junk_index, order):
     j = 0
     good_now = 0    # the number of good image (good prediction)
     nJunk = 0       # the number of junk image
-    for i_order, order_i in enumerate(order):
-        flag = False
+    for i_order , order_i in enumerate(order):
+        flag = False    # good/junk index, default "junk"
         if good_index[good_index == order_i].size != 0:
             cmc[i_order - nJunk:] = 1
             flag = True
@@ -61,19 +61,35 @@ def compute_AP(good_index, junk_index, order):
 
 
 def compute_video_cmc_map(distMat, q_pids, g_pids, q_camids, g_camids, junk_idx=None, max_rank=5):
+    """
+    params:
+
+    distMat: 2-d array_like, size: M x N (M: number of probe, N: Number of gallary)\\
+    q_pids: array_like, size: M; query person ID\\
+    g_pids: array_like, size: N; gallary person ID \\
+    q_camids: array_like, size: M; query camera ID \\
+    g_camids: array_like, size: N; gallary camera ID \\
+    junk_idx: \\
+    mark_rank: 
+
+    return:
+
+    cmc: array_like, size: N; Cumulative Match Characteristics \\
+    map: scalar; mean Average Precision 
+    """
     probe_num = q_pids.size
     gallery_num = g_pids.size
-    max_rank = max_rank if gallery_num >= max_rank else gallery_num
+    max_rank = max_rank if gallery_num >= max_rank else gallery_num 
     assert distMat.shape[0] == probe_num and distMat.shape[1] == gallery_num
     assert q_camids.size == probe_num and g_camids.size == gallery_num
 
-    ap = np.zeros(probe_num, np.float32)
-    cmc = np.zeros((probe_num, gallery_num), np.float32)
-    for i_p in range(probe_num):
+    ap = np.zeros(probe_num, np.float32)    # multiple query AP; size M
+    cmc = np.zeros((probe_num, gallery_num), np.float32)    # multiple query CMC; size: M x N
+    for i_p in range(probe_num):    # the i_p ^th probe image
         dist = distMat[i_p, ...]
-        p_id = q_pids[i_p]
-        cam_p = q_camids[i_p]
-        pos = np.where(g_pids == p_id)[0]
+        p_id = q_pids[i_p]  # probe person id
+        cam_p = q_camids[i_p]   # 
+        pos = np.where(g_pids == p_id)[0]   # np.where(condition) returns condition satisfies.
         pos2 = np.where(g_camids[pos] != cam_p)
         good_index = pos[pos2]
         pos3 = np.where(g_camids[pos] == cam_p)
@@ -81,7 +97,7 @@ def compute_video_cmc_map(distMat, q_pids, g_pids, q_camids, g_camids, junk_idx=
         junk_index = temp_junk_index if junk_idx is None else np.concatenate(
             (junk_idx, temp_junk_index), axis=0)
         dist_order = np.argsort(dist)
-        ap[i_p, ...], cmc[i_p, ...] = compute_AP(good_index, junk_index, dist_order)
+        ap[i_p, ...], cmc[i_p, ...] = compute_AP(good_index, junk_index, dist_order) # recall "compute_AP" function
 
     cmc = np.sum(cmc, axis=0) / probe_num
     mAP = np.sum(ap) / ap.size
